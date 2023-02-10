7927ce570226bbee40feabf59e2b833094cc8d7b
Mikepro1211 <miguelalexander890@gmail.com>
2023-02-09 21:33:43 -0600

    primer commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Real Madrid</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Inicio</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Historia</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Champions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Ligas</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="card" style="width: 85rem; height: 35rem;">
+  <img  id="realmadrid" src="https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/og-image.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">El real de mis amores</h5>
+    <p class="card-text">No hay ningun otro equipo  con la misma gloria que nosotros<p>
+    <a href="#" class="btn btn-primary">Ver champions</a>
+  </div>
+</div>
+
+</body>
+</html>
